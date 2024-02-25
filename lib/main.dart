f99498f1e267d4dc9bfafb107e3fa2217a77f6d9
DBG <danielbernardograset@gmail.com>
2024-02-25 12:47:48 +0100

    Actualització de la visualització del codi a l'arxiu .dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'screens/home.dart';
 
+// main() -> permet l'inici de l'execució de l'app
 void main() => runApp(App());
 
 class App extends StatelessWidget {
 
-  // visualització bàsica de l'app
+  // construeix visualització bàsica de l'app
   Widget build(BuildContext context) {
 
     return new MaterialApp(
